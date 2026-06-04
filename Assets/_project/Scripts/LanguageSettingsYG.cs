3faa8e16894ed0fba80b3551b1bc1804944d17a3
Firstrex13 <seregka001@gmail.com>
2026-06-04 15:02:44 +0300

    добавил локализацию

---
+++ Assets/_project/Scripts/LanguageSettingsYG.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+
+public class LanguageSettingsYG : MonoBehaviour
+{
+    private void Start()
+    {
+       // YG2.SwitchLanguage("en");
+    }
+}
