0cf98830cfcd7978c3a629953a528f57c4d167a1
Firstrex13 <seregka001@gmail.com>
2026-06-18 12:21:30 +0300

    Добавил еще один уровень и их выбор в меню

---
+++ Assets/_project/Scripts/Scenes.cs
@@ -0,0 +1,7 @@
+public static class Scenes 
+{
+    public const string BOOT_SCENE = "BootScene";
+    public const string MAIN_MENU = "MainMenu";
+    public const string LEVEL1 = "Level1";
+    public const string LEVEL2 = "Level2";
+}
--- Assets/_project/Scripts/UI/MenuHandler.cs
+++ Assets/_project/Scripts/UI/MenuHandler.cs
@@ -1,23 +1,52 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _levelUpPanel;
     [SerializeField] private GameObject _menuPanel;
     [SerializeField] private GameObject _settingsPanel;
+    [SerializeField] private GameObject _levelsPanel;
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _title;
-    [SerializeField] private StatsUpgraderOnButton[] _statsUpgraders;
+    [SerializeField] private TextMeshProUGUI _activeLevelNumberText;
+    [SerializeField] private Image[] _activeLevelImages;
+
+    private string _activeScene;
+
+    private void Start()
+    {
+        if(_activeScene == null)
+        {
+            _activeScene = Scenes.LEVEL1;
+        }
+
+        ChooseActiveScene(_activeScene);
+    }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("GameplayScene");
+        SceneManager.LoadScene(_activeScene);
     }
 
-    public void QuitGame()
+    public void ChooseActiveScene(string sceneName)
     {
-        Application.Quit();
+        _activeScene = sceneName;
+
+        if (_activeScene == Scenes.LEVEL1)
+        {
+            _activeLevelNumberText.text = "1";
+            _activeLevelImages[1].color = Color.gray;
+            _activeLevelImages[0].color = Color.green;
+        }
+        else if (_activeScene == Scenes.LEVEL2)
+        {
+            _activeLevelNumberText.text = "2";
+            _activeLevelImages[1].color = Color.green;
+            _activeLevelImages[0].color = Color.gray;
+        }
     }
 
     public void OpenSkillsPenal()
@@ -46,6 +75,13 @@ public class MenuHandler : MonoBehaviour
             _player.gameObject.SetActive(true);
             _title.SetActive(true);
         }
+        else if (_levelsPanel.activeSelf)
+        {
+            _levelsPanel.SetActive(false);
+            _menuPanel.SetActive(true);
+            _player.gameObject.SetActive(true);
+            _title.SetActive(true);
+        }
     }
 
     public void OpenSettingsPanel()
@@ -54,5 +90,13 @@ public class MenuHandler : MonoBehaviour
         _title.SetActive(false);
         _player.gameObject.SetActive(false);
         _settingsPanel.SetActive(true);
+    }
+
+    public void OpenLevelsPanel()
+    {
+        _menuPanel.SetActive(false);
+        _title.SetActive(false);
+        _player.gameObject.SetActive(false);
+        _levelsPanel.SetActive(true);
     }
 }
